6508378be6518835e5aaa834d55b251f00e31e40
Rainier Raydan <rainierraydan@Rainiers-Air.fibertel.com.ar>
2025-01-27 12:03:28 -0300

    Modificacion4

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
 document.getElementById('runButton').addEventListener('click', async () => {
-    try {
-      const response = await fetch('/pages/api/airtable-fetch'); // Endpoint de tu API en Vercel
-      const data = await response.json();
-      
-      console.log(JSON.stringify(data, null, 2)); // Muestra datos en consola
-      // Puedes manipular 'data' como necesites
-    } catch (error) {
-      console.error('Error al buscar datos:', error);
-    }
-  });
+    console.log('111');    
+});
