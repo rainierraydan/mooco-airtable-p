782b33fb37d9a4283c7395f493999b9e686aa4a6
Rainier Raydan <rainierraydan@Rainiers-Air.fibertel.com.ar>
2025-01-27 15:22:26 -0300

    Modificacion13

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
+const auth = require('basic-auth');
 const express = require('express');
 const path = require('path');
 const { getTableData } = require('./airtable');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware de autenticación básica
+app.use((req, res, next) => {
+    const user = auth(req);
+    const username = process.env.AUTH_USERNAME || 'admin'; // Cambia 'admin' por tu usuario
+    const password = process.env.AUTH_PASSWORD || 'miclave'; // Cambia 'miclave' por tu clave
+  
+    if (!user || user.name !== username || user.pass !== password) {
+      res.set('WWW-Authenticate', 'Basic realm="Acceso restringido"');
+      return res.status(401).send('Acceso no autorizado');
+    }
+    next();
+  });
 
 // Servir archivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
